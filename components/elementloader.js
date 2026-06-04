0a5f921ddb03ac17903d17797c23f0c139de8da6
CarZoneDB <72553562+CarZoneDB@users.noreply.github.com>
2026-06-04 14:49:17 +0200

    Update elementloader.js

--- components/elementloader.js
+++ components/elementloader.js
@@ -26,7 +26,7 @@ const path = window.location.pathname;
 
 const isSubPage =
   path.includes('/cars/') ||
-  path.includes('/races/');
+  path.includes('/races/') ||
   path.includes('/calculator/');
 
 const componentPath = isSubPage
